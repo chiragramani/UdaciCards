1d9dc829323d517dbe2045503eb201c51648ff53
Chirag Ramani <chirag.ramani7@gmail.com>
2019-04-01 21:41:39 +0530

    Abstracted AsyncStorage in api.js to fetch decks list and update deck

---
+++ utils/api.js
@@ -0,0 +1,19 @@
+import { AsyncStorage } from "react-native";
+const DECKS_STORAGE_KEY = "UdaciCards:Decks";
+
+export function updateDeck({ deckName, deck }) {
+  return AsyncStorage.mergeItem(
+    DECKS_STORAGE_KEY,
+    JSON.stringify({
+      [deckName]: deck
+    })
+  );
+}
+
+function formatDeckResults(results) {
+  return results === null ? {} : JSON.parse(results);
+}
+
+export function fetchDecks() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(formatDeckResults);
+}

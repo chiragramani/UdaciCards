36315c5b58e15ab96973967697db2b30d0344a11
Chirag Ramani <chirag.ramani7@gmail.com>
2019-04-01 19:37:27 +0530

    Added status bar

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import Tabs from './components/Navigator'
+import React from "react";
+import { View, StatusBar } from "react-native";
+import Tabs from "./components/Navigator";
+import { Constants } from "expo";
+
+function UdaciStatusBar() {
+  return (
+    <View style={{ height: Constants.statusBarHeight }}>
+      <StatusBar translucent />
+    </View>
+  );
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <Tabs />
+      <View style={{ flex: 1 }}>
+        <UdaciStatusBar />
+        <Tabs />
+      </View>
     );
   }
 }

3a53ff8206bf0279a28e25ef33c1762d51dc3a1a
Chirag Ramani <chirag.ramani7@gmail.com>
2019-04-04 00:17:28 +0530

    Showing questions in quiz

--- components/Quiz.js
+++ components/Quiz.js
@@ -7,11 +7,45 @@ import {
   ScrollView
 } from "react-native";
 import { connect } from "react-redux";
+import { blue, white, red, green } from "../utils/colors";
 
 class Quiz extends Component {
+  state = {
+    score: 0,
+    questionIndex: 0,
+    showResult: false,
+    showAnswer: false
+  };
+
+  handleAnswer = (isCorrect) => {
+    const { deck } = this.props;
+    const { questions } = deck;
+    const { questionIndex } = this.state;
+    const totalQuestions = questions.length;
+    if (questionIndex === totalQuestions - 1) {
+      /// Current is the last question
+      this.setState(previousState => ({
+        score: isCorrect ? previousState.score + 1 : previousState.score,
+        showResult: true
+      }));
+    } else {
+      /// Move to next question...
+      this.setState(previousState => ({
+        questionIndex: previousState.questionIndex + 1,
+        score: isCorrect ? previousState.score + 1 : previousState.score,
+        showResult: false,
+        showAnswer: false
+      }));
+    }
+  };
+
   render() {
     const { deck } = this.props;
     const { questions } = deck;
+    const { questionIndex } = this.state;
+    const currentDisplayableIndex = questionIndex + 1;
+    const totalQuestions = questions.length;
+    const question = questions[questionIndex];
     return (
       <ScrollView style={styles.container}>
         {questions.length === 0 && (
@@ -19,6 +53,27 @@ class Quiz extends Component {
             Sorry you cannot take this quiz since there are no cards in the
             deck.
           </Text>
+        )}
+
+        {questions.length > 0 && (
+          <View>
+            <Text style={[styles.text, { textAlign: "left" }]}>
+              {currentDisplayableIndex} / {totalQuestions}
+            </Text>
+            <Text style={styles.text}>{question.question}</Text>
+            <TouchableOpacity
+              onPress={() => this.handleAnswer(true)}
+              style={styles.button}
+            >
+              <Text style={[styles.buttonText, styles.text]}>Correct</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.handleAnswer(false)}
+              style={[styles.button, styles.incorrectButton]}
+            >
+              <Text style={[styles.buttonText, styles.text]}>Incorrect</Text>
+            </TouchableOpacity>
+          </View>
         )}
       </ScrollView>
     );
@@ -39,12 +94,15 @@ const styles = StyleSheet.create({
     padding: 16,
     paddingLeft: 32,
     paddingRight: 32,
-    backgroundColor: "#4933FF",
+    backgroundColor: green,
     borderRadius: 6,
     marginTop: 32
   },
   buttonText: {
-    color: "white"
+    color: white
+  },
+  incorrectButton: {
+    backgroundColor: red
   }
 });
 
--- utils/colors.js
+++ utils/colors.js
@@ -1,3 +1,4 @@
 export const blue = "#4933FF";
 export const white = "#fff";
 export const red = "#FF0000";
+export const green = '#008000';
